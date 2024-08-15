7dda198c224b4d59cd406eec93af95673996a38e
Saihaj Mann <saihajmann07@gmail.com>
2024-08-14 19:12:12 -0600

    Update flashcards.js

--- src/component/flashcards.js
+++ src/component/flashcards.js
@@ -9,7 +9,10 @@ export default function FlashCards() {
     return (
         <div>
             <p>Flash Cards</p>
-            <input/>
+            <input 
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}/>
             <textarea/>
 
         </div>
