e3e724fc86f8252d92ab3fa803322ac14a0459be
Saihaj Mann <saihajmann07@gmail.com>
2024-08-15 16:05:00 -0600

    changes

--- src/component/flashcards.js
+++ src/component/flashcards.js
@@ -6,18 +6,35 @@ export default function FlashCards() {
     const [cards, setCards] = useState([])
     const [name, setName] = useState('')
     const [description, setDescription] = useState('')
+
+    function addCard (){
+        if(name == ""){
+            return
+        }
+        if(description=""){
+            return
+        }
+        const card = {"name":name, "description":description}
+        setCards((othercards) => [...prevSearches, card]);
+    }
+
     return (
         <div>
             <p>Flash Cards</p>
             <input 
                 type="text"
+                defaultValue={"Card name or answer"}
                 value={name}
                 onChange={(e) => setName(e.target.value)}/>
+            <br/>
             <textarea
                 type="text"
+                defaultValue={"Card defination or problem"}
                 value={description}
                 onChange={(e) => setDescription(e.target.value)}/>
-
+            <br/>
+            <button onClick={() => setCardsk()}>Add card</button>
+            
         </div>
     )
 }
